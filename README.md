221db730e4419f2d4f610c178fb4fd4c68fd6a3c
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-11-17 20:25:02 -0700

    Finish make change app

---
+++ README.md
@@ -0,0 +1,41 @@
+## Make Change Project
+### Week Two Homework for Skill Distillery
+
+#### Overview
+
+An simple java application that simulates a cash register. 
+
+when the user runs the application they will be prompted to enter the items price.
+
+```
+$ Price of the item: 
+```
+after entering the items price the user will then be prompted to enter the amount they paid.
+ 
+```
+$ Amount pay: 
+```
+The application will then print out the change due to the user by how many of each piece of currency.
+
+for example, the user enters 3.96 for the item price and paid with 20 and the application prints out. 
+
+```
+1 $10.0
+1 $5.0
+1 $1.0
+4 $0.01
+```
+
+#### Concepts
+
+* Methods
+* Loops
+* Algorithms
+
+#### Technologies Used
+
+ * Java
+
+#### Lessons Learned
+
+Learn the basic structure of the java language using methods, loops, and variables.
--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static double[] changeType = { 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01 };
 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Price of the item: ");
+		double price = input.nextDouble();
+		System.out.println("Amount pay: ");
+		double amount = input.nextDouble();
+		if (amount == price) {
+			promptUserGoodday();
+		} else if (amount < price) {
+			promptUserNotEnough();
+		} else {
+			giveChange(price, amount);
+			promptUserGoodday();
+		}
+		input.close();
+	}
+
+	public static void promptUserGoodday() {
+		System.out.println("Thank you have a good day!");
+	}
+
+	public static void promptUserNotEnough() {
+		System.out.println("Not enough!!");
+	}
+
+	public static void giveChange(double price, double amount) {
+		double change = (amount - price) + 0.001;
+		for (int i = 0; i < changeType.length; i++) {
+			if (change >= changeType[i]) {
+				amount = (int) (change / changeType[i]);
+				change -= amount * changeType[i];
+				System.out.println((int) amount + " $" + changeType[i]);
+			}
+		}
 	}
 
 }
